1e646bfd422ddbbc5fdf31cb27ce6c73e1277836
David Eccles (gringer) <bioinformatics@gringene.org>
2016-07-30 11:00:07 +1200

    Initial Commit -- Government data slurp + very basic explore app

---
+++ app.R
@@ -0,0 +1,48 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+govdata.df <- read.csv("/bioinf/govhack/data/govdata/nalafs-jun2015/nalafs-jun2015-tables-csv.csv.gz", stringsAsFactors = FALSE);
+councilNames <- unique(govdata.df$Series_title_1);
+dataCats <- unique(govdata.df$Series_title_2);
+
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("GovHack Application"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("council",label = "Council",
+                         choices=councilNames),
+      selectInput("cat",
+                  label = "Category", dataCats)
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("dataPlot")
+    )
+  )
+);
+
+server <- shinyServer(function(input, output) {
+  
+  output$dataPlot <- renderPlot({
+    
+    data.sub.df <- subset(govdata.df, (Series_title_1 == input$council) & (Series_title_2 == input$cat));
+    data.units <- head(data.sub.df$UNITS,1);
+    plot(Data_value ~ Period, data=data.sub.df, ylab=data.units);
+
+  });
+  
+});
+
+
+shinyApp(ui = ui, server = server);
