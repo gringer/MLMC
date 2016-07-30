3c2a9b3a6a0e9cf5b63f95489cd668a51ed9742f
David Eccles (gringer) <bioinformatics@gringene.org>
2016-07-30 14:45:58 +1200

    Different graphs for Time series / last year comparisons

--- server.R
+++ server.R
@@ -6,17 +6,24 @@ excludeCouncilNames <- (c("Auckland Tourism, Events and Economic Development", "
 
 govdata.df <- subset(govdata.df, !(Series_title_1 %in% excludeCouncilNames));
 
+lastYear.val <- "2015.06";
+
 councilNames <- unique(govdata.df$Series_title_1);
 dataCats <- unique(govdata.df$Series_title_2);
 
 shinyServer(function(input, output, session) {
   
   output$dataPlot <- renderPlot({
-    
-    data.sub.df <- subset(govdata.df, (Series_title_1 == input$council) & (Series_title_2 == input$cat));
-    data.units <- head(data.sub.df$UNITS,1);
-    plot(data.sub.df$Period, data.sub.df$Data_value, ylab=data.units);
-
+    if(input$dataType == "Over Time"){
+      data.sub.df <- subset(govdata.df, (Series_title_1 == input$council) & (Series_title_2 == input$cat));
+      data.units <- head(data.sub.df$UNITS,1);
+      plot(data.sub.df$Period, data.sub.df$Data_value, ylab=data.units, type="l");
+    }
+    if(input$dataType == "Last Year"){
+      data.sub.df <- subset(govdata.df, (Period == lastYear.val) & (Series_title_2 == input$cat));
+      data.units <- head(data.sub.df$UNITS,1);
+      barplot(Data_value ~ Period, data=data.sub.df, ylab=data.units, type="l");
+    }
   });
   
   ## Observers to detect button changes and switch tabs
