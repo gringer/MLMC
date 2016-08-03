15ce9f0ed492a03c2f9c7d00a2b56693a6f661d7
David Eccles (gringer) <bioinformatics@gringene.org>
2016-08-03 22:26:59 +1200

    Additional usage graphs, more annotation

--- stats/MLMC-stats/server.R
+++ stats/MLMC-stats/server.R
@@ -7,15 +7,36 @@ library(tidyr);
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  data.df <- read.csv("../../logs/accesslog.csv");
+  data.df <- read.csv("../../logs/accesslog.csv", stringsAsFactors=FALSE);
   data.df$time <- as.POSIXct(data.df$time);
   data.wide.df <- spread(data = data.df, key = "inputCategory", value="value");
   data.wide.df$hours <- round(data.wide.df$time, "hours");
+  data.wide.df$viewButton <- as.numeric(data.wide.df$viewButton);
 
-  output$distPlot <- renderPlot({
+  output$timePlot <- renderPlot({
     htable <- table(as.character(round(data.wide.df$hours,"hours")));
-    plot(x=as.POSIXct(names(htable)), y=htable, xlab="Time", ylab = "Number of uses", yaxt="n");
+    plot(x=as.POSIXct(names(htable)), y=htable, xlab="Time",
+         ylab = "Number of uses", yaxt="n", type="b");
     axis(2);
+  });
+
+  output$catPlot <- renderPlot({
+    par(mar=c(5,10,1,1));
+    barplot(sort(table(data.wide.df$cat)), xlab="Graphs Viewed",
+            cex.names=0.75, horiz = TRUE, las=1);
+  });
+
+  output$councilPlot <- renderPlot({
+    par(mar=c(5,10,1,1));
+    barplot(sort(table(data.wide.df$council)), xlab="Graphs Viewed",
+            cex.names=0.75, horiz = TRUE, las=1);
+  })
+  
+  output$dwellPlot <- renderPlot({
+    dwellCount <- table(tapply(data.wide.df$viewButton, data.wide.df$requestID,
+                         max));
+    barplot(dwellCount, xlab = "Graphs viewed per visit",
+            ylab = "Number of visits");
   })
   
 })
--- stats/MLMC-stats/ui.R
+++ stats/MLMC-stats/ui.R
@@ -5,23 +5,14 @@ library(shiny);
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+  titlePanel("Usage Statistics for MLMC"),
+  fluidRow(
+    column(6,plotOutput("timePlot")),
+    column(6,plotOutput("dwellPlot"))
+  ),
+  fluidRow(
+    column(6,plotOutput("catPlot")),
+    column(6,plotOutput("councilPlot"))
   )
-))
+));
